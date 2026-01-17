49b3571d686b8b71fcf62a9dfa72ea3bb354cc3a
Badru <badruakfm@gmail.com>
2026-01-17 14:26:49 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const tips = [
+    { id: 1, title: "Minum air putih yang cukup setiap hari" },
+    { id: 2, title: "Olahraga ringan minimal 30 menit" },
+    { id: 3, title: "Tidur cukup 7–8 jam per malam" },
+    { id: 4, title: "Kurangi konsumsi gula berlebih" },
+    { id: 5, title: "Perbanyak konsumsi buah dan sayur" },
+    { id: 6, title: "Kelola stres dengan baik" },
+    { id: 7, title: "Jaga pola makan teratur" },
+    { id: 8, title: "Hindari begadang terlalu sering" }
+];
+
 const bmiHistory = [];
 const seenIds = new Set(); // buat deteksi duplikasi opsional
 
@@ -51,6 +62,27 @@ app.post("/api/v1/bmi/sync", (req, res) => {
 // cek isi memory
 app.get("/api/v1/bmi", (_req, res) => {
     res.json({ ok: true, data: bmiHistory, total: bmiHistory.length });
+});
+
+app.get("/api/v1/tips", (_req, res) => {
+    res.json({
+        ok: true,
+        data: tips,
+        total: tips.length
+    });
+});
+
+// =========================
+// GET 1 tips random
+// =========================
+app.get("/api/v1/tips/random", (_req, res) => {
+    const randomIndex = Math.floor(Math.random() * tips.length);
+    const tip = tips[randomIndex];
+
+    res.json({
+        ok: true,
+        data: tip
+    });
 });
 
 const PORT = process.env.PORT || 3000;
